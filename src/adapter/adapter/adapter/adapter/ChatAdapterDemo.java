77faa78dcfedf55530bfdd8111754587ce5deb27
SatimAskhat <s23067494@narxoz.kz>
2025-03-07 12:28:15 +0500

    Update ChatAdapterDemo.java

--- src/adapter/adapter/adapter/adapter/ChatAdapterDemo.java
+++ src/adapter/adapter/adapter/adapter/ChatAdapterDemo.java
@@ -1,1 +1,11 @@
+package adapter;
+
+public class ChatAdapterDemo {
+    public static void main(String[] args) {
+        LegacyChatService legacyChatService = new LegacyChatService();
+        ChatService chatService = new ChatServiceAdapter(legacyChatService);
+
+        chatService.sendMessage("Hello world!");
+    }
+}
 
